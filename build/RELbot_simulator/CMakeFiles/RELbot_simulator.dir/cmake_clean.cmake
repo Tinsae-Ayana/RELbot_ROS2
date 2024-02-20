41cffef10f5364487bd89508171bae8915388229
Tinsae-Ayana <tinsaeaeroastro@gmail.com>
2024-02-20 03:53:09 +0100

    launch file added

---
+++ build/RELbot_simulator/CMakeFiles/RELbot_simulator.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RELbot_simulator.dir/src/DynamicsSimulation.cpp.o"
+  "CMakeFiles/RELbot_simulator.dir/src/DynamicsSimulation.cpp.o.d"
+  "CMakeFiles/RELbot_simulator.dir/src/RELbot_simulator.cpp.o"
+  "CMakeFiles/RELbot_simulator.dir/src/RELbot_simulator.cpp.o.d"
+  "RELbot_simulator"
+  "RELbot_simulator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RELbot_simulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/RELbot_simulator/CMakeFiles/RELbot_simulator.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for RELbot_simulator.
+# This may be replaced when dependencies are built.
